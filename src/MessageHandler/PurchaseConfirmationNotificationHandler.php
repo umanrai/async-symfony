436ec910e71618d492a6904d455178e7132a9f62
umanrai <umanrai58@gmail.com>
2024-05-17 16:48:05 +0545

    updated .gitignore

--- src/MessageHandler/PurchaseConfirmationNotificationHandler.php
+++ src/MessageHandler/PurchaseConfirmationNotificationHandler.php
@@ -3,18 +3,34 @@
 namespace App\MessageHandler;
 
 use App\Message\PurchaseConfirmationNotification;
+use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
+use Symfony\Component\Mime\Email;
 
 
 #[AsMessageHandler]
 class PurchaseConfirmationNotificationHandler
 {
+
+    public function __construct(private MailerInterface $mailer)
+    {
+    }
+
     public function __invoke(PurchaseConfirmationNotification $notification): Void
     {
         // 1. Create a PDF contract note
         echo 'Creating a PDF contract note...<br>';
 
         // 2. Email the contract note to buyer
-        echo 'Emailing the contract note...' . $notification->getOrder()->getBuyer()->getEmail() . '<br>';
+//        echo 'Emailing contract note to...' . $notification->getOrder()->getBuyer()->getEmail() . '<br>';
+
+        $email = (new Email())
+            ->from('sales@stocksapp.com')
+            ->to($notification->getOrder()->getBuyer()->getEmail())
+            ->subject('Contract note for order' .$notification->getOrder()->getId())
+            ->text('Here is your contract note');
+
+        $this->mailer->send($email);
+
     }
 }
