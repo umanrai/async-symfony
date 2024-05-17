4e79786a1c5cd8c3a42ede0f2a549d18b9f03164
umanrai <umanrai58@gmail.com>
2024-05-17 12:53:32 +0545

    create PurchaseConfirmationNotification

---
+++ src/Message/PurchaseConfirmationNotification.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Message;
+
+class PurchaseConfirmationNotification {
+
+    public function __construct(private object $order)
+    {
+
+    }
+
+    public function getOrder(): object
+    {
+        return $this->order;
+    }
+}
